5f860476f2217d6ce2c429c7b9eef7f924cbe583
oshogaur <osho.g@devtrust.biz>
2020-12-15 19:34:42 +0530

    second

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+//initializing express
 const express = require('express');
 const app = express();
+//including that courses file in our page
+const courses = require('./courses');
+//use function is used to include that function in our page
+app.use('/courses',courses);
 
+//routing 
 app.get('/',function(req,res){
 
-    res.send("this is my first page");
+    res.send("welcome to my website");
 
 });
 
